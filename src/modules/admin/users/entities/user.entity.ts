bdee8447d3ce1e3affec341144c6b3ca730820ae
Bismark Socompi <bismark.sr.17@gmail.com>
2025-06-30 07:06:05 -0400

    update 30-06-2025

---
+++ src/modules/admin/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({unique: true})
+    username: string
+
+    @Column({unique: true})
+    email: string
+
+    @Column()
+    password: string
+
+    @Column({default: true})
+    isActive: boolean
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
